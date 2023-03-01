c31da05578ada1c27df0154d368a9f39c6758646
Ciemny21 <jakub.olejnik@student.wat.edu.pl>
2023-03-01 15:59:04 +0100

    Init

---
+++ src/main/java/com/example/firstproject/ExampleController.java
@@ -0,0 +1,33 @@
+package com.example.firstproject;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
+
+public class ExampleController {
+    @FXML
+    private Circle myCircle;
+    private double x;
+    private double y;
+
+
+    public void up(ActionEvent e){
+        //.out.println("Up");
+        myCircle.setCenterY(y-=10);
+    }
+    public void down(ActionEvent e){
+        //System.out.println("Down");
+        myCircle.setCenterY(y+=10);
+    }
+    public void left(ActionEvent e){
+        //System.out.println("Left");
+        myCircle.setCenterX(x-=10);
+    }
+    public void right(ActionEvent e){
+        //System.out.println("Right");
+        myCircle.setCenterX(x+=10);
+    }
+}
